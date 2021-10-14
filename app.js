4202a9de7344c4f3b1381532d13fd9e65171f6d7
andrii <y@.com>
2021-10-14 15:19:01 +0300

    make it better

--- app.js
+++ app.js
@@ -10,12 +10,7 @@ const joke_url = config.joke_url;
 
 const options = {
     webHook: {
-      // Port to which you should bind is assigned to $PORT variable
-      // See: https://devcenter.heroku.com/articles/dynos#local-environment-variables
       port: process.env.PORT
-      // you do NOT need to set up certificates since Heroku provides
-      // the SSL certs already (https://<app-name>.herokuapp.com)
-      // Also no need to pass IP because on Heroku you need to bind to 0.0.0.0
     }
 };
 
@@ -27,53 +22,26 @@ bot.setWebHook(`${url}/bot${TOKEN}`);
 async function getHtml(joke_url){
     return axios.get(joke_url).then(res => res.data);
 }
+function getJoke($){
+    const data = $('p')[0].children;
+    let joke = "";
+    data.forEach(Node => {
+        if(Node.data){
+            joke += Node.data;
+            joke += '\n';
+        }   
+    });
 
+    return joke;
+}
 bot.onText(/\/joke/, (msg) => {
     const chatId = msg.chat.id;
     getHtml(joke_url)
         .then(html => {
             const $ = cheerio.load(html);
-                    
-            const data = $('p')[0].children;
-            let joke = "";
-            data.forEach(Node => {
-                if(Node.data){
-                    joke += Node.data;
-                    joke += '\n';
-                }   
-            }); 
+            const joke = getJoke($);
+             
             bot.sendMessage(chatId, joke);
         })
         .catch(err => console.log(err));
 });
-/*bot.on("message", (msg) => {
-    bot.sendMessage(msg.chat.id, "Hello");
-});*/
-
-
-
-/*
-bot.onText(/\/joke/, (msg) => {
-
-    const chatId = msg.chat.id;
-
-    axios(joke_url)
-        .then(res => {
-            const html = res.data;
-            const $ = cheerio.load(html);
-            
-            const _data = $('p')[0].children;
-            let joke = "";
-            _data.forEach(Node => {
-                if(Node.data){
-                    joke += Node.data;
-                    joke += '\n';
-                }   
-            });
-            bot.sendMessage(chatId, joke);
-        }).catch((err) => {
-            console.log(err)
-        });
-
-        
-});*/
